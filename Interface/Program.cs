5424d5fc94e9cc176a23f4c5308bcdd1eeca4068
Peter Elorm Seth Selormey <pesselormey@st.ug.edu.gh>
2025-08-05 11:33:10 +0000

    Add initial project structure with IMovable interface and Car/Bicycle implementations

---
+++ Interface/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OOP_Interfaces
+{
+    // Interface
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Car class
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Bicycle class
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();       // Output: Car is moving
+            bicycle.Move();   // Output: Bicycle is moving
+        }
+    }
+}
