22d1a1f0e081a4407305003f936345c82637a550
Peter Elorm Seth Selormey <pesselormey@st.ug.edu.gh>
2025-08-05 11:27:27 +0000

    Implement basic structure for OOP inheritance with Animal, Dog, and Cat classes

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace OOP_Inheritance
+{
+    // Base class
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class Dog
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class Cat
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+            genericAnimal.MakeSound();  // Output: Some generic sound
+            dog.MakeSound();            // Output: Bark
+            cat.MakeSound();            // Output: Meow
+        }
+    }
+}
