3bb6987d05f68d6c7dbc3f94d243a33c39d30f72
Peter Elorm Seth Selormey <pesselormey@st.ug.edu.gh>
2025-08-05 11:28:29 +0000

    Add Inheritance.dll and refint for .NET 9.0 build
    - Generated Inheritance.dll and its reference assembly for .NET 9.0.
    - Included necessary metadata and assembly attributes.
    - Updated project structure to support .NET 9.0 compatibility.

--- Inheritance/obj/Debug/net9.0/Inheritance.AssemblyInfo.cs
+++ Inheritance/obj/Debug/net9.0/Inheritance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22d1a1f0e081a4407305003f936345c82637a550")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
